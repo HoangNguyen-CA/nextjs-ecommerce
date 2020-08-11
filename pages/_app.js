8298aaf31c7c9b1528d91746939ad18478de829f
HoangNguyen-CA <hoang.viet.nguyen@outlook.com>
2020-08-11 18:38:21 -0400

    added context-api to app to maintain state

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CartContext from '../components/CartContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CartContext.Provider value={{ cart: [] }}>
+      <Component {...pageProps} />
+    </CartContext.Provider>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import { useContext } from 'react';
+
 import Head from 'next/head';
 import Layout from '../components/Layout';
 import { Button } from 'reactstrap';
+import '../components/CartContext';
+import CartContext from '../components/CartContext';
 
 export default function Home() {
+  const context = useContext(CartContext);
   return (
     <Layout>
       <Head>
